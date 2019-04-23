396aff87e7a35deab30485995ee2f13178a9a326
CosmicDisorder <mike.ghazi.harris@gmail.com>
2019-04-23 10:54:26 -0500

    added date, recreated table, reversed order of blogs shown in index

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -12,10 +13,12 @@ class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200))
+    date = db.Column(db.Date())
     body = db.Column(db.String(4000))
 
-    def __init__(self, title, body):
+    def __init__(self, title, date, body):
         self.title = title
+        self.date = date
         self.body = body
 
 @app.route('/')
@@ -29,12 +32,14 @@ def index():
 def new_entry():
     if request.method == 'POST':
         title = request.form['title']
+        today = str(date.today())
         body = request.form['body']
-        blog_entry = Blog(title, body)
+        
+        blog_entry = Blog(title, today, body)
         db.session.add(blog_entry)
         db.session.commit()
         return redirect('/')
-        
+
     return render_template('new-blog.html', title="Add a Blog Entry")
 
 
