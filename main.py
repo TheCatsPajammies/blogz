13c997f8a10f3484d7d4fcd5461cc64892d570e4
CosmicDisorder <mike.ghazi.harris@gmail.com>
2019-05-09 10:07:05 -0500

    can has blogz db

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from datetime import date
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:badgerbadger@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:itmanyblogz@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'asdf1234lkj987'
